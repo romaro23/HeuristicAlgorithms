5bca62504e28bd3e00aabbf94bb7fade4b355edb
Romaro <Romaro@ROMARO-PC>
2026-09-23 22:53:23 +0300

    feat: implement Hill Climbing algorithm with 2-opt neighborhood
    - Added `get_neighbour` function to generate modified routes by reversing a random sub-route.
    - Implemented `hill_climbing` logic with a `max_stuck` stopping criteria to prevent infinite loops at local minima.

--- tsp_hillclimbing.py
+++ tsp_hillclimbing.py
@@ -30,6 +30,29 @@ def evaluate_function(route: list, distances: np.ndarray) -> float:
 
     return total_dist
 
+def get_neighbour(route: list) -> list:
+    i, j = sorted(random.sample(range(len(route)), 2))
+    return route[:i] + route[i:j][::-1] + route[j:]
+
+def hill_climbing(distances: np.ndarray, initial_route: list, max_stuck: int = 500) -> tuple[list, float]:
+    best_route = initial_route[:]
+    best_score = evaluate_function(best_route, distances)
+
+    stuck_counter = 0
+
+    while stuck_counter < max_stuck:
+        next_route = get_neighbour(best_route)
+        next_score = evaluate_function(next_route, distances)
+
+        if next_score < best_score:
+            best_route = next_route[:]
+            best_score = next_score
+            stuck_counter = 0
+        else:
+            stuck_counter += 1
+
+    return best_route, best_score
+
 filepath = r"D:\PycharmProjects\University\HeuristicAlgorithms\data\berlin52.tsp"
 problem = tsplib95.load(filepath)
 
@@ -39,4 +62,5 @@ N = problem.dimension
 distances = calculate_distances(cities, N)
 
 test_list = list(range(52))
-print(evaluate_function(test_list, distances))
+random.shuffle(test_list)
+print(hill_climbing(distances, test_list))
